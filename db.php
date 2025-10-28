61e171b01802311d9b7aa671bc39733c9f3b205d
nahlol <103286897+whitewolfgaming1@users.noreply.github.com>
2025-10-27 22:32:56 -0500

    Upload files
    Empty files
    Changes to "createuser," "db," and "auth"

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php
+//sets up database
+$DB_HOST = "localhost";
+$DB_NAME = "wiki";
+$DB_USER = "INFX472";
+$DB_PASS = "P*ssword";
+
+$mysqli = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_Name);
+?>
