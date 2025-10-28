c1b110483ded7e65dd1eb87db0c5a4fa34d20deb
Ashley Rourk <ashleyrourk1@gmail.com>
2025-10-28 12:28:17 -0500

    Updated addarticle.php

--- addarticle.php
+++ addarticle.php
@@ -1,6 +1,34 @@
 <?php 
 
 include 'db.php';
+if($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
+// checks if form is submitted
+if(isset($_POST['submit'])) {
+    // gets submitted values
+    if(isset($_FILES['image'])) {
+        $imagename = $_FILES['image']['name'];
+        $imagetmp = $_FILES['image']['tmp_name'];
+        $title = $mysqli->real_escape_string($_POST["title"]);
+        $intro = $mysqli->real_escape_string($_POST["intro"]);
+        $body = $mysqli->real_escape_string($_POST["body"]);
+        $references = $mysqli->real_escape_string($_POST["references"]);
+
+        $sql = "INSERT INTO article (image, title, intro, body, `reference`) VALUES ('$imagename', '$title', '$intro', '$body', '$references')";
+    }
+    else {
+        $title = $mysqli->real_escape_string($_POST["title"]);
+        $intro = $mysqli->real_escape_string($_POST["intro"]);
+        $body = $mysqli->real_escape_string($_POST["body"]);
+        $references = $mysqli->real_escape_string($_POST["references"]);
+
+        $sql = "INSERT INTO article (title, intro, body, `reference`) VALUES ('$title', '$intro', '$body', '$references')";
+    }
+
+    $submit = $mysqli->query($sql);
+}
 
 ?>
 
@@ -16,10 +44,10 @@ include 'db.php';
             <a href="index.php" id="return">Go back home</a>
             <p>Use the form below to add a new article to the wiki.</p>
         </div>
-        <form method="POST">
+        <form method="POST" enctype="multipart/form-data" action="addarticle.php">
             <div class="form-input" id="image-upload">
                 <label for="image">Image:</label><br>
-                <input type="file" id="image" accept="image/*">
+                <input type="file" id="image" accept="image/*" name="image">
             </div>
             <div class="form-input">
                 <label for="title">Title:</label><br>
@@ -35,10 +63,10 @@ include 'db.php';
              </div>
              <div>
                 <label for="references">References:</label><br>
-                <textarea id="section3" name="section3" rows="4" cols="50"></textarea>
+                <textarea id="references" name="references" rows="4" cols="50"></textarea>
              </div>
              <!-- reference section, image, date, user uploaded -->
-            <input type="submit" value="Add Article">
+            <input type="submit" value="Add Article" name="submit">
         </form>
     </body>
 </html>
